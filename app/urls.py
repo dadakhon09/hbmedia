67ea7707e0baed06546838da8154464b63caa5b5
dadakhon09 <odadaxon27@mail.ru>
2019-11-07 14:23:05 +0500

    s

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import index, about, blog, contact, portfolio, services, single_blog, send_mail
+from .views import index, about, blog, contact, portfolio, services, single_blog, send_mail, video_create, \
+	content_create, web_create
 
 urlpatterns = [
 	path('', index, name='index'),
@@ -8,6 +9,9 @@ urlpatterns = [
 	path('contact/', contact, name='contact'),
 	path('portfolio/', portfolio, name='portfolio'),
 	path('services/', services, name='services'),
+	path('web/', web_create, name='web-create'),
+	path('content/', content_create, name='content-create'),
+	path('video/', video_create, name='video-create'),
 	path('single_blog/', single_blog, name='single_blog'),
 	path('sendmail/', send_mail, name='sendmail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,37 +1,60 @@
 from django.shortcuts import render, redirect
 from django.core.mail import EmailMessage
 
+
 def index(request):
-	return render(request, 'index.html', {})
+    return render(request, 'index.html', {})
+
 
 def about(request):
-	return render(request, 'about.html', {})
+    return render(request, 'about.html', {})
+
 
 def blog(request):
-	return render(request, 'blog.html', {})
+    return render(request, 'blog.html', {})
+
 
 def contact(request):
-	full_name = request.POST.get('full-name')
-	email = request.POST.get('email')
-	organization = request.POST.get('organization')
-	phone = request.POST.get('phone')
-	message = request.POST.get('message')
+    full_name = request.POST.get('full-name')
+    email = request.POST.get('email')
+    organization = request.POST.get('organization')
+    phone = request.POST.get('phone')
+    message = request.POST.get('message')
 
-	m = EmailMessage('contact request', f'From: {full_name}\n Email: {email}\n Organization: {organization}\n Phone: {phone}\n Message:\n{message}\n', 'odadaxon99@gmail.com', ['khasanboevbobur@gmail.com', ])
-	m.send()
-	return render(request, 'contact.html', {})
+    m = EmailMessage('contact request',
+                     f'From: {full_name}\n Email: {email}\n Organization: {organization}\n Phone: {phone}\n Message:\n{message}\n',
+                     'odadaxon99@gmail.com', ['khasanboevbobur@gmail.com', ])
+    m.send()
+    return render(request, 'contact.html', {})
+
 
 def portfolio(request):
-	return render(request, 'portfolio.html', {})
+    return render(request, 'portfolio.html', {})
+
 
 def services(request):
-	return render(request, 'services.html', {})
+    return render(request, 'services.html', {})
+
+
+def web_create(request):
+    return render(request, 'web-create.html', {})
+
+
+def content_create(request):
+    return render(request, 'content-create.html', {})
+
+
+def video_create(request):
+    return render(request, 'video-create.html', {})
+
 
 def single_blog(request):
-	return render(request, 'single-blog.html', {})
+    return render(request, 'single-blog.html', {})
+
 
 def send_mail(request):
-	mail = request.POST.get('nl-email')
-	s = EmailMessage('new subscriber', f'{mail} has just subscribed', 'odadaxon99@gmail.com', ['khasanboevbobur@gmail.com', ])
-	s.send()
-	return redirect('index')
+    mail = request.POST.get('nl-email')
+    s = EmailMessage('new subscriber', f'{mail} has just subscribed', 'odadaxon99@gmail.com',
+                     ['khasanboevbobur@gmail.com', ])
+    s.send()
+    return redirect('index')
