152b7228ef6a7c5a758087591f8401fa3544cda2
dadakhon09 <odadaxon27@mail.ru>
2019-11-10 23:54:50 +0500

    localization added

--- hbmedia/urls.py
+++ hbmedia/urls.py
@@ -1,9 +1,20 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.utils import translation
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += i18n_patterns(
     path('', include('app.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
