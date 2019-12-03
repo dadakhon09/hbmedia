66c7b11bab4b8a24a852d25f5dc63809cd3258ad
dadakhon09 <odadaxon27@mail.ru>
2019-12-03 15:46:01 +0500

    a

--- hbmedia/settings.py
+++ hbmedia/settings.py
@@ -153,4 +153,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'odadaxon99@gmail.com'
-EMAIL_HOST_PASSWORD = 'xmrepxzvsbzclyfm'
+EMAIL_HOST_PASSWORD = 'lfaslawbqbzwafvj'
