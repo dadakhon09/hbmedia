730862a9d92338b53e6c5de682048b9f093cb9b4
root <root@c4513b6682c7>
2020-02-29 17:20:49 +0300

    s

--- hbmedia/settings.py
+++ hbmedia/settings.py
@@ -153,4 +153,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hbmediauz@gmail.com'
-EMAIL_HOST_PASSWORD = 'eminfrcsebpghebq'
+EMAIL_HOST_PASSWORD = 'ywclhwfnwvtgmzrb'
